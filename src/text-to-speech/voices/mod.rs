4d4e6d7ec9ec2c4e4f4ae3f591a7b404e3ed93df
kawaki-san <dev@kanjala.com>
2022-07-10 23:29:35 +0200

    refactor: [tts] move voices methods to voices module

--- src/text-to-speech/voices/mod.rs
+++ src/text-to-speech/voices/mod.rs
@@ -1,9 +1,15 @@
+use hyper::{
+    body::Buf,
+    header::{HeaderValue, AUTHORIZATION},
+    Body, Client, Method, Request, StatusCode,
+};
 use serde::{Deserialize, Serialize};
 
 mod errors;
 pub use errors::*;
+use url::Url;
 
-use super::custom_models::CustomModel;
+use super::{custom_models::CustomModel, TextToSpeech};
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
 pub struct Voice {
     #[serde(rename = "url")]
@@ -24,7 +30,7 @@ pub struct Voice {
     pub customization: Option<Box<CustomModel>>,
 }
 
-#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Default, Serialize, Deserialize)]
 pub struct SupportedFeatures {
     #[serde(rename = "custom_pronunciation")]
     pub custom_pronunciation: bool,
@@ -168,6 +174,100 @@ impl WatsonVoice {
             WatsonVoice::ZhCnLiNa => "zh-CN_LiNaVoice",
             WatsonVoice::ZhCnWangWei => "zh-CN_WangWeiVoice",
             WatsonVoice::ZhCnZhangJing => "zh-CN_ZhangJingVoice",
+        }
+    }
+}
+
+impl TextToSpeech<'_> {
+    pub async fn list_voices(&self) -> Result<Vec<Voice>, ListVoicesError> {
+        let mut url = Url::parse(&self.service_url).unwrap();
+        Self::set_voices_path(&mut url);
+        let req = Request::builder()
+            .uri(url.to_string())
+            .header(
+                AUTHORIZATION,
+                HeaderValue::from_str(&format!("Bearer {}", self.access_token)).unwrap(),
+            )
+            .method(Method::GET)
+            .body(Body::empty())
+            .map_err(|e| ListVoicesError::ConnectionError(e.to_string()))?;
+        let https = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_only()
+            .enable_http1()
+            .build();
+        let client = Client::builder().build(https);
+        let response = client
+            .request(req)
+            .await
+            .map_err(|e| ListVoicesError::ConnectionError(e.to_string()))?;
+        match response.status() {
+            StatusCode::OK => {
+                #[derive(Deserialize)]
+                struct Root {
+                    voices: Vec<Voice>,
+                }
+                let body = hyper::body::aggregate(response).await.unwrap();
+                let root: Root = serde_json::from_reader(body.reader()).unwrap();
+
+                Ok(root.voices)
+            }
+            StatusCode::NOT_ACCEPTABLE => Err(ListVoicesError::NotAcceptable),
+            StatusCode::UNSUPPORTED_MEDIA_TYPE => Err(ListVoicesError::UnsupportedMediaType),
+            StatusCode::INTERNAL_SERVER_ERROR => Err(ListVoicesError::InternalServerError),
+            StatusCode::SERVICE_UNAVAILABLE => Err(ListVoicesError::ServiceUnavailable),
+            _ => {
+                unreachable!()
+            }
+        }
+    }
+
+    fn set_voices_path(uri: &mut Url) {
+        uri.set_path("v1/voices");
+    }
+
+    pub async fn get_voice(
+        &self,
+        voice: WatsonVoice,
+        customisation_id: Option<&str>,
+    ) -> Result<Voice, GetVoiceError> {
+        let mut url = Url::parse(&self.service_url).unwrap();
+        Self::set_voices_path(&mut url);
+        url.set_query(customisation_id);
+        let req = Request::builder()
+            .uri(format!("{}/{}", url, voice.id()))
+            .header(
+                AUTHORIZATION,
+                HeaderValue::from_str(&format!("Bearer {}", self.access_token)).unwrap(),
+            )
+            .method(Method::GET)
+            .body(Body::empty())
+            .map_err(|e| GetVoiceError::ConnectionError(e.to_string()))?;
+        let https = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_only()
+            .enable_http1()
+            .build();
+        let client = Client::builder().build(https);
+        let response = client
+            .request(req)
+            .await
+            .map_err(|e| GetVoiceError::ConnectionError(e.to_string()))?;
+        assert_eq!(response.status(), 200);
+        match response.status() {
+            StatusCode::OK => {
+                let body = hyper::body::aggregate(response).await.unwrap();
+                let root: Voice = serde_json::from_reader(body.reader()).unwrap();
+
+                Ok(root)
+            }
+            StatusCode::NOT_ACCEPTABLE => Err(GetVoiceError::NotAcceptable),
+            StatusCode::UNSUPPORTED_MEDIA_TYPE => Err(GetVoiceError::UnsupportedMediaType),
+            StatusCode::INTERNAL_SERVER_ERROR => Err(GetVoiceError::InternalServerError),
+            StatusCode::SERVICE_UNAVAILABLE => Err(GetVoiceError::ServiceUnavailable),
+            _ => {
+                unreachable!()
+            }
         }
     }
 }
