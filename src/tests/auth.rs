ac23b898754ad1c43cb3f0dfa9af0572b99d82f7
kawaki-san <dev@kanjala.com>
2022-07-17 19:21:39 +0200

    chore: fix auth test

--- src/tests/auth.rs
+++ src/tests/auth.rs
@@ -1,22 +1,23 @@
+use reqwest::{
+    header::{HeaderValue, CONTENT_TYPE},
+    Body, Client, Method, Request, Url,
+};
+
 #[tokio::test]
 async fn http1_iam_key() {
     let me = "api_key";
     let url = Url::parse("https://iam.cloud.ibm.com/identity/token").unwrap();
-    let req = Request::builder()
-        .uri(url.to_string())
-        .method(Method::POST)
-        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
-        .body(Body::from(format!(
-            "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={}",
-            me
-        )))
-        .unwrap();
-    let https = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_native_roots()
-        .https_only()
-        .enable_http1()
-        .build();
-    let client = Client::builder().build(https);
-    let c = client.request(req).await.unwrap();
+    let mut req = Request::new(Method::POST, url);
+    let headers = req.headers_mut();
+    headers.insert(
+        CONTENT_TYPE,
+        HeaderValue::from_static("application/x-www-form-urlencoded"),
+    );
+    *req.body_mut() = Some(Body::from(format!(
+        "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={}",
+        me
+    )));
+    let client = Client::new();
+    let c = client.execute(req).await.unwrap();
     assert_eq!(c.status(), 400);
 }
